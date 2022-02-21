7d29285334c61ccfd21e75f5a3831d419e442116
Jang-NH <55skgus@naver.com>
2022-02-21 23:10:57 +0900

    README.md

---
+++ src/test/java/com/jnh/mj/MjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnh.mj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
