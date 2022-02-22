46e360892e600dc00adb3260659a4513ddcc96e5
Jang-NH <55skgus@naver.com>
2022-02-22 23:04:03 +0900

    MainController.java, index.html

---
+++ src/main/java/com/jnh/mj/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.jnh.mj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return index();
+    }
+}
