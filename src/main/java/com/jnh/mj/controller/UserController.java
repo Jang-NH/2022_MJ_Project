3b2c3fc85bb40c7fc3aada29f2859140b00f4fe1
Jang-NH <55skgus@naver.com>
2022-03-01 23:21:37 +0900

    controller

--- src/main/java/com/jnh/mj/controller/UserController.java
+++ src/main/java/com/jnh/mj/controller/UserController.java
@@ -1,7 +1,28 @@
 package com.jnh.mj.controller;
 
+import com.jnh.mj.dto.UserSaveDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/user/*")
+@RequiredArgsConstructor
 public class UserController {
+
+    // 회원가입 폼
+    @GetMapping("save")
+    public String saveForm() {
+        return "user/save";
+    }
+
+    // 회원가입
+    @GetMapping("save")
+    public String save(@ModelAttribute UserSaveDTO userSaveDTO) {
+
+        return null;
+    }
+
 }
