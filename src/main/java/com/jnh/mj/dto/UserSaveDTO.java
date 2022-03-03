0bbd1b4fef6f3c8fb9d603774df0947c1b561ba7
Jang-NH <55skgus@naver.com>
2022-03-04 07:29:33 +0900

    UserManagement

--- src/main/java/com/jnh/mj/dto/UserSaveDTO.java
+++ src/main/java/com/jnh/mj/dto/UserSaveDTO.java
@@ -8,4 +8,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserSaveDTO {
+    private String userEmail;
+    private String userPassword;
+    private String userNickname;
 }
--- src/main/java/com/jnh/mj/entity/UserEntity.java
+++ src/main/java/com/jnh/mj/entity/UserEntity.java
@@ -1,9 +1,12 @@
 package com.jnh.mj.entity;
 
+import com.jnh.mj.dto.UserSaveDTO;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Getter @Setter
@@ -12,6 +15,43 @@ public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "userId")
+    @Column(name = "user_id")
     private Long id;
+
+    @NotNull
+    @Column(length = 20)
+    private String platformType;
+
+    @Column(length = 20)
+    private String accessToken;
+
+    @NotNull
+    @Column(length = 50, unique = true)
+    private String userEmail;
+
+    @NotNull
+    @Column(length = 50)
+    private String userPassword;
+
+    @Column(length = 200)
+    private String userProfilename;
+
+    @NotNull
+    @Column(length = 50, unique = true)
+    private String userNickname;
+
+    @Column(length = 20)
+    private String userMbti;
+
+    public static UserEntity toSaveUser(UserSaveDTO userSaveDTO) {
+        UserEntity userEntity = new UserEntity();
+
+        userEntity.setUserEmail(userSaveDTO.getUserEmail());
+        userEntity.setUserPassword(userSaveDTO.getUserPassword());
+        userEntity.setUserNickname(userSaveDTO.getUserNickname());
+
+        return userEntity;
+    }
+
+
 }
