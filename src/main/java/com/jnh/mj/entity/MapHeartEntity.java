df0b5d34344df72117119019e75728df1dde9357
Jang-NH <55skgus@naver.com>
2022-03-12 23:31:26 +0900

    MapBoard Entity

--- src/main/java/com/jnh/mj/entity/MapHeartEntity.java
+++ src/main/java/com/jnh/mj/entity/MapHeartEntity.java
@@ -1,4 +1,19 @@
 package com.jnh.mj.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "board_heart_table")
 public class MapHeartEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "heart_number")
+    public Long id;
+
 }
--- src/main/java/com/jnh/mj/entity/MarkerEntity.java
+++ src/main/java/com/jnh/mj/entity/MarkerEntity.java
@@ -1,4 +1,19 @@
 package com.jnh.mj.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "board_marker_table")
 public class MarkerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "marker_number")
+    public Long id;
+
 }
