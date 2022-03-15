8a48be0d4c8f60e88eddc01546c83e1cb944d56b
Jang-NH <55skgus@naver.com>
2022-03-15 23:01:37 +0900

    MapBoard

--- src/main/java/com/jnh/mj/controller/BoardController.java
+++ src/main/java/com/jnh/mj/controller/BoardController.java
@@ -1,9 +1,12 @@
 package com.jnh.mj.controller;
 
+import com.jnh.mj.dto.BoardSaveDTO;
 import com.jnh.mj.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,6 +18,11 @@ public class BoardController {
     private final BoardService bs;
 
     // 글 작성 폼
+    @GetMapping("save")
+    public String saveForm(Model model) {
+        model.addAttribute("board", new BoardSaveDTO());
+        return "board/save";
+    }
 
     // 구글맵 마커 저장 글작성
 
