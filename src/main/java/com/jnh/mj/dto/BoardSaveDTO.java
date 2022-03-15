4198f4249377f289311d8635ec1c811ffbd1d1ca
Jang-NH <55skgus@naver.com>
2022-03-15 23:06:43 +0900

    MapBoard

--- src/main/java/com/jnh/mj/dto/BoardSaveDTO.java
+++ src/main/java/com/jnh/mj/dto/BoardSaveDTO.java
@@ -1,4 +1,20 @@
 package com.jnh.mj.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BoardSaveDTO {
+    private String boardWriter;
+    private String boardTitle;
+    private String boardContents;
+    private long markerLatitude;
+    private long markerLongitude;
+    private String boardHits;
+    private String saveTime;
+
+
 }
